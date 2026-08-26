b9a72208184eb73797a9807fc025c9de15037865
utsav568 <sutsav644@gmail.com>
2026-08-27 00:36:59 +0530

    code

--- pattern/patternprint2/pattern3.cpp
+++ pattern/patternprint2/pattern3.cpp
@@ -15,5 +15,6 @@ int main() {
             cout<<q;
         }
         cout<<endl;
+        
     }
 }
--- pattern/patternprint2/pattern4.cpp
+++ pattern/patternprint2/pattern4.cpp
@@ -12,7 +12,8 @@ int main() {
             cout<<"";
         
 
-        cout<<endl;
+        
     }
+    cout<<endl;
 }
 }
